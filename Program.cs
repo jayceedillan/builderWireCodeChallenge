43a0720e0bc4697772cc369cd7fde885c95cb2b5
jfurigay <jessie.furigay@author-it.com>
2022-06-11 15:21:57 +0800

    upload files

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using BuilderWireChallenge.Utilities;
+
+IService service = new Service();
+
+string[] articles = service.readFiles("Article.txt");
+string[] words = service.readFiles("Words.txt");
+var listOfResults = new List<string>();
+int alphabetIdx = 0;
+int addAlphabet = 1;
+
+foreach (string word in words)
+{
+
+    string alphabetChar = service.getAlphabet(ref alphabetIdx, ref addAlphabet);
+    List<int> listOfParagraphIdx = new List<int>();
+
+    int findWords = service.matchWords(articles, word);
+
+    int articleIdx = 1;
+    foreach (var article in articles)
+    {
+
+        int matchWordsCount = service.matchWords(article, word);
+        if (matchWordsCount != 0)
+        {
+            if (matchWordsCount != 1)
+            {
+                for (var i = 0; i < matchWordsCount; i++)
+                {
+                    listOfParagraphIdx.Add(articleIdx);
+                }
+            }
+            else
+            {
+                listOfParagraphIdx.Add(articleIdx);
+            }
+        }
+        articleIdx++;
+    }
+
+
+    string output = $"{alphabetChar.ToString().ToLower()}. {word} {{{findWords}:";
+    string separateCommaByIdx = string.Join(",", listOfParagraphIdx);
+    output += $"{separateCommaByIdx}}}";
+    listOfResults.Add(output);
+}
+
+if (service.writeTextFile(listOfResults))
+{
+    Console.WriteLine("Theres ann error while writing the files");
+}
+
---
+++ Utilities/Service.cs
@@ -0,0 +1,106 @@
+namespace BuilderWireChallenge.Utilities
+{
+    public class Service : IService
+    {
+        string currentDirectory =
+               Directory.GetCurrentDirectory() + "/Files/";
+
+        public IEnumerable<char>? getAllAlphabet()
+        {
+            return Enumerable.Range(0, 26).Select(i => Convert.ToChar('A' + i));
+        }
+
+        public string getAlphabet(ref int idx, ref int needToAdd)
+        {
+
+            int tempIdx = idx >= 26 ? 0 : idx;
+            string alphabet = this.getAllAlphabet()!.ElementAt(tempIdx).ToString().ToLower();
+
+
+            if (idx >= 26)
+            {
+                needToAdd++;
+                idx = 1;
+            }
+            else
+            {
+                idx++;
+            }
+            string alphabets = "";
+
+            for (var i = 0; i < needToAdd; i++)
+            {
+                alphabets += alphabet;
+            }
+
+            return alphabets;
+
+        }
+
+        ///<summary>
+        /// 
+        ///</summary>
+        public int matchWords(string article, string searchWord)
+        {
+            int matchWordsCount = searchingMatchWords(article, searchWord);
+
+            return matchWordsCount;
+        }
+
+        public int matchWords(string[] articles, string searchWord)
+        {
+            string newArticles = String.Join(",", articles.Select(a => a.ToString()).ToArray()).ToLower();
+            int matchWordsCount = searchingMatchWords(newArticles, searchWord);
+
+            return matchWordsCount;
+        }
+
+
+        private int searchingMatchWords(string article, string searchWord)
+        {
+            string[] newArticles = article.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var matchWords = from word in newArticles
+                             where word.Equals(searchWord, StringComparison.InvariantCultureIgnoreCase)
+                             select word;
+
+            if (matchWords.Count() == 0 && searchWord.Contains("."))
+            {
+                string[] searchArticleAgain = article.Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries);
+
+                matchWords = searchArticleAgain.Where(a => a.ToLower().Contains(searchWord.ToLower())).AsEnumerable();
+                return matchWords.Count();
+
+            }
+            return matchWords.Count();
+        }
+        public string[] readFiles(string fileName)
+        {
+            return File.ReadAllLines(currentDirectory + fileName);
+        }
+
+        public bool writeTextFile(List<string> listOfResults)
+        {
+            string writePathDir = currentDirectory + "Output.txt";
+
+            try
+            {
+                using (StreamWriter sw = File.CreateText(writePathDir))
+                {
+                    foreach (var result in listOfResults)
+                    {
+                        sw.WriteLine(result);
+                    }
+                }
+
+                return false;
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex);
+                return true;
+            }
+        }
+    }
+
+}
